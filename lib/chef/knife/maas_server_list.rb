30645b88fb6697fa8eac2c6bfff84127af7bb2df
JJ Asghar <jj@getchef.com>
2015-02-12 15:35:43 -0600

    added some better UI

--- lib/chef/knife/maas_server_list.rb
+++ lib/chef/knife/maas_server_list.rb
@@ -15,7 +15,7 @@ class Chef
           ui.color('Status', :bold),
           ui.color('Power State', :bold),
           ui.color('Hostname', :bold),
-          ui.color('OS', :bold),
+          ui.color('Operating System', :bold),
           ui.color('Owner', :bold),
           ui.color('IP Address', :bold)
         ]
@@ -25,8 +25,30 @@ class Chef
           Chef::Log.debug("Server: #{server.to_yaml}")
 
           server_list << server['system_id'].to_s
-          server_list << server['status'].to_s
-          server_list << server['power_state'].to_s
+          server_list << begin
+                           status = server['status'].to_s
+                           case status
+                           when "6"
+                             ui.color("deploying", :yellow)
+                           when "4"
+                             ui.color("ready", :green)
+                           when "0"
+                             ui.color("unknown", :red)
+                           else
+                             ui.color(state, :green)
+                           end
+                         end
+          server_list <<  begin
+                           power_state = server['power_state'].to_s
+                           case power_state
+                           when "off"
+                             ui.color(power_state, :red)
+                           when "on"
+                             ui.color(power_state, :green)
+                           else
+                             ui.color(power_state, :yellow)
+                           end
+                          end
           server_list << server['hostname'].to_s
           server_list << server['osystem'].to_s
           server_list << server['owner'].to_s
