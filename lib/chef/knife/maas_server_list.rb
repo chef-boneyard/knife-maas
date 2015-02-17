601a85bb0f559aba6288a8f50908b8ae9533e427
JJ Asghar <jj@getchef.com>
2015-02-17 11:50:01 -0600

    Updated the server list notifications

--- lib/chef/knife/maas_server_list.rb
+++ lib/chef/knife/maas_server_list.rb
@@ -25,18 +25,34 @@ class Chef
 
           server_list << server['system_id'].to_s
           server_list << begin
-                           status = server['status'].to_s
+                           status = server['substatus'].to_s
                            case status
-                           when "6"
+                           when "2","3","8","11","13","15"
+                             ui.color("failed", :red)
+                           when "0","1","5","7","9","10","12","14"
                              ui.color("deploying", :yellow)
-                           when "4"
-                             ui.color("ready", :green)
-                           when "0"
-                             ui.color("unknown", :red)
+                           when "4","6"
+                             ui.color("deployed", :green)
                            else
                              ui.color(state, :green)
                            end
                          end
+          # NEW = 0
+          # COMMISSIONING = 1
+          # FAILED_COMMISSIONING = 2
+          # MISSING = 3
+          # READY = 4
+          # RESERVED = 5
+          # DEPLOYED = 6
+          # RETIRED = 7
+          # BROKEN = 8
+          # DEPLOYING = 9
+          # ALLOCATED = 10
+          # FAILED_DEPLOYMENT = 11
+          # RELEASING = 12
+          # FAILED_RELEASING = 13
+          # DISK_ERASING = 14
+          # FAILED_DISK_ERASING = 15
           server_list <<  begin
                            power_state = server['power_state'].to_s
                            case power_state
