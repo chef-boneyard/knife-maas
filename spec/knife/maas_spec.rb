2ad4ef9baf6cec0c8f08f042fdff6f8ec1c7f6a4
cmluciano <cmluciano@dragoon.io>
2015-02-26 22:09:49 -0500

    Make sure all tests pass

--- spec/knife/maas_spec.rb
+++ spec/knife/maas_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
-describe Knife::Maas do
+describe Chef::Knife::Maas do
   it 'has a version number' do
-    expect(Knife::Maas::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(Chef::Knife::Maas::VERSION).not_to be nil
   end
 end
